8109445aacc4825460ce133a5377045b2ffec4bb
ARK9001 <aish2012@gmail.com>
2016-07-19 00:00:08 -0700

    Commit

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -64,7 +64,75 @@ public class ListSorter<T> {
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+        //return null;
+		
+		if(list.size() == 1)
+        	return list;
+
+		int s1 = 0;
+		int s2 = 0;
+		List<T> split1 = list.subList(0, list.size()/2);
+        List<T> split2 = list.subList(list.size()/2, list.size());
+        List<T> retList = new LinkedList<T>();
+
+
+        split1 = mergeSort(split1, comparator);
+        split2 = mergeSort(split2, comparator);
+
+        T part1 = split1.get(s1);
+        T part2 = split2.get(s2);
+
+        while(retList.size() < list.size())
+        {
+
+        	if(comparator.compare(part2, part1) > 0)
+        	{
+        		retList.add(part1);
+        		
+        		if(s1 == split1.size())
+        		{
+        			retList.add(part2);
+        			
+        			if(s2 < split2.size()){
+        				retList.add(split2.get(s2));
+        			}
+
+        			s2 = s2 + 1;
+        		}
+
+        		else
+        		{
+        			part1 = split1.get(s1);
+        		}
+
+        		s1 = s1 + 1;
+        	}
+        	else
+        	{
+        		retList.add(part2);
+        	
+        		if(s2 == split2.size())
+        		{
+        			retList.add(part1);
+        			
+        			if(s1 < split1.size()){
+        				retList.add(split1.get(s1));
+        			}
+
+        			s1 = s1 + 1;
+        		}
+        		
+        		else
+        		{
+        			part2 = split2.get(s2);
+        		}
+
+        		s2 = s2 + 1;
+        	}
+        }
+
+        return retList;
+
 	}
 
 	/**
@@ -76,6 +144,20 @@ public class ListSorter<T> {
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
+         PriorityQueue<T> newHeap = new PriorityQueue<T>();
+
+        for(T val : list)
+        {
+        	newHeap.offer(val);
+        }
+
+        list.clear();
+
+        while(!newHeap.isEmpty())
+        {
+        	list.add(newHeap.poll());
+        }
+
 	}
 
 	
@@ -90,7 +172,37 @@ public class ListSorter<T> {
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+        //return null;
+		T var;
+
+   		List<T> retK = new LinkedList<T>();
+
+        PriorityQueue<T> newHeap = new PriorityQueue<T>();
+     
+    
+
+		for(T val : list)
+        {
+			if(newHeap.size() < k)
+        		newHeap.offer(val);
+        	else
+        	{
+        		var = newHeap.poll();
+        		
+        		if(comparator.compare(val, var) > 0)
+        			newHeap.offer(val);
+        		else
+        			newHeap.offer(var);
+        	}
+        }
+
+		while(!newHeap.isEmpty())
+		{
+			retK.add(newHeap.poll());
+		}
+
+		return retK;
+
 	}
 
 	
